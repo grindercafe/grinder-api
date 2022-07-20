1201af0c7b84f4e7365a379a4ead4ffeac54661d
mub-pro <mubarakbakrman@gmail.com>
2022-07-20 17:17:42 +0300

    event routes and controller

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,9 +2,65 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    //
+    public function index()
+    {
+        return Event::all();
+    }
+    
+    public function show($id)
+    {
+        return Event::findOrFail($id);
+    }
+
+    public function store(Request $request)
+    {
+        $event = [
+            'date'=> $request->date,
+            'start_time'=> $request->start_time,
+            'end_time'=> $request->end_time,
+            'singer_name'=> $request->singer_name,
+            'singer_img'=> $request->singer_img,
+            'available_chairs'=> $request->available_chairs,
+            'price_per_person'=> $request->price_per_person
+        ];
+        
+        $createdEvent = Event::create($event);
+        
+        return response()->json([
+            'success'=> true,
+            'message'=> 'event created successfully',
+            'data'=> $createdEvent
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $event = Event::findOrFail($id);
+
+        $event->update($request->all());
+
+        return response()->json([
+            'success'=> true,
+            'message'=> 'event updated successfully',
+            'data'=> $event
+        ]);
+    }
+
+    public function delete($id)
+    {
+        $event = Event::findOrFail($id);
+
+        $event->destroy($id);
+
+        return response()->json([
+            'success'=> true,
+            'message'=> 'event deleted successfully',
+            'data'=> $event
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/events',[EventController::class, 'index'])->name('event.index');
+Route::get('/events/{id}',[EventController::class, 'show'])->name('event.show');
+Route::post('/event',[EventController::class, 'store'])->name('event.store');
+Route::delete('/events/{id}',[EventController::class, 'delete'])->name('event.delete');
+Route::put('/events/{id}', [EventController::class, 'update'])->name('event.update');
