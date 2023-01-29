ef12e82a8e6a8aa927145f80ce6c2c51c669ebc8
mub-pro <mubarakbakrman@gmail.com>
2023-01-29 03:00:48 +0300

    secure the api with key

---
+++ app/Http/Middleware/VerifyApiKey.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class VerifyApiKey
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $api_key = $request->api_key;
+        
+        if(!$api_key) {
+            return response()->json([
+                'status_code'=> 401,
+                'message'=> 'You Must Provide an Api Key!'
+            ], 401);
+        }
+        
+        $is_valid = DB::table('api_keys')->where('key', $api_key)->exists();
+
+        if(!$is_valid) {
+            return response()->json([
+                'status_code'=> 401,
+                'message'=> 'Invalid Api Key!'
+            ], 401);
+        }
+
+        return $next($request);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -20,69 +20,62 @@ use App\Http\Controllers\CustomerController;
 |
 */
 
-// paginated events
-Route::get('/events', [EventController::class, 'index']);
-// visible events only
-Route::get('/visible_events', [EventController::class, 'visible_events']);
-// all events orderd by date
-Route::get('/allEvents', [EventController::class, 'allEvents']);
+Route::middleware('verify_api_key')->group(function () {
+    // paginated events
+    Route::get('/events', [EventController::class, 'index']);
+    // visible events only
+    Route::get('/visible_events', [EventController::class, 'visible_events']);
+    // all events orderd by date
+    Route::get('/allEvents', [EventController::class, 'allEvents']);
 
-Route::get('/events/{id}', [EventController::class, 'show']);
-Route::get('/visible_events/{id}', [EventController::class, 'visible_event']);
+    Route::get('/events/{id}', [EventController::class, 'show']);
+    Route::get('/visible_events/{id}', [EventController::class, 'visible_event']);
 
-Route::post('/event', [EventController::class, 'store']);
+    Route::post('/event', [EventController::class, 'store']);
 
-Route::put('/events/{id}', [EventController::class, 'update']);
+    Route::put('/events/{id}', [EventController::class, 'update']);
 
-Route::delete('/events/{id}', [EventController::class, 'delete']);
+    Route::delete('/events/{id}', [EventController::class, 'delete']);
 
-Route::get('/events/{id}/bookings', [EventController::class, 'bookings_by_event']);
+    Route::get('/events/{id}/bookings', [EventController::class, 'bookings_by_event']);
 
-Route::patch('/events/{id}/update_visibility', [EventController::class, 'update_is_visible']);
+    Route::patch('/events/{id}/update_visibility', [EventController::class, 'update_is_visible']);
 
-Route::post('/events/{id}/hide_tables', [EventController::class, 'hide_tables']);
+    Route::post('/events/{id}/hide_tables', [EventController::class, 'hide_tables']);
 
-Route::get('/customers', [CustomerController::class, 'index']);
-Route::get('/customers/{id}', [CustomerController::class, 'show']);
-Route::post('/customer', [CustomerController::class, 'store']);
-Route::put('/customers/{id}', [CustomerController::class, 'update']);
-Route::delete('/customers/{id}', [CustomerController::class, 'delete']);
-Route::get('/customers/{id}/bookings', [CustomerController::class, 'bookings_by_customer']);
+    Route::get('/customers', [CustomerController::class, 'index']);
+    Route::get('/customers/{id}', [CustomerController::class, 'show']);
+    Route::post('/customer', [CustomerController::class, 'store']);
+    Route::put('/customers/{id}', [CustomerController::class, 'update']);
+    Route::delete('/customers/{id}', [CustomerController::class, 'delete']);
+    Route::get('/customers/{id}/bookings', [CustomerController::class, 'bookings_by_customer']);
 
-// Route::get('/status', [BookingStatusController::class, 'index']);
-// Route::get('/status/{id}', [BookingStatusController::class, 'show']);
-// Route::post('/status', [BookingStatusController::class, 'store']);
-// Route::put('/status/{id}', [BookingStatusController::class, 'update']);
-// Route::delete('/status/{id}', [BookingStatusController::class, 'delete']);
+    Route::get('/bookings', [BookingController::class, 'index']);
+    Route::get('/bookings/{booking:uuid}', [BookingController::class, 'show']);
+    Route::get('/bookings/dashboard/{id}', [BookingController::class, 'showInDashboard']);
+    Route::post('/booking', [BookingController::class, 'store']);
+    Route::post('/test_booking', [BookingController::class, 'testStore']);
 
-Route::get('/bookings', [BookingController::class, 'index']);
-Route::get('/bookings/{booking:uuid}', [BookingController::class, 'show']);
-Route::get('/bookings/dashboard/{id}', [BookingController::class, 'showInDashboard']);
-Route::post('/booking', [BookingController::class, 'store']);
-Route::post('/test_booking', [BookingController::class, 'testStore']);
+    Route::put('/update_event_in_booking/{id}', [BookingController::class, 'updateRelatedEvent']);
+    Route::put('/update_payment_status_in_booking/{id}', [BookingController::class, 'updatePaymentStatus']);
 
-Route::put('/update_event_in_booking/{id}', [BookingController::class, 'updateRelatedEvent']);
-Route::put('/update_payment_status_in_booking/{id}', [BookingController::class, 'updatePaymentStatus']);
+    Route::delete('/bookings/{id}', [BookingController::class, 'delete']);
+    Route::patch('/bookings/{id}/cancel', [BookingController::class, 'cancel']);
+    Route::patch('/bookings/{id}/updateMessageStatus', [BookingController::class, 'messageStatus']);
 
-Route::delete('/bookings/{id}', [BookingController::class, 'delete']);
-Route::patch('/bookings/{id}/cancel', [BookingController::class, 'cancel']);
-Route::patch('/bookings/{id}/updateMessageStatus', [BookingController::class, 'messageStatus']);
-
-Route::put('/bookings/{id}/updateTables', [BookingController::class, 'update_tables']);
+    Route::put('/bookings/{id}/updateTables', [BookingController::class, 'update_tables']);
 
 
-Route::get('/tables', [TableController::class, 'index']);
+    Route::get('/tables', [TableController::class, 'index']);
 
-Route::post('/login', [LoginController::class, 'login']);
-Route::post('/logout', [LoginController::class, 'logout']);
+    Route::post('/login', [LoginController::class, 'login']);
+    Route::post('/logout', [LoginController::class, 'logout']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::post('/payment', [PaymentController::class, 'createInvoice']);
+    Route::get('/result', [PaymentController::class, 'result']);
+    Route::get('/update_payment_status', [PaymentController::class, 'updatePaymentStatus']);
 });
 
-Route::post('/payment', [PaymentController::class, 'createInvoice']);
-Route::get('/result', [PaymentController::class, 'result']);
-Route::get('/update_payment_status', [PaymentController::class, 'updatePaymentStatus']);
 
 // Route::post('test_payment', [PaymentController::class, 'createTestInvoice']);
 // Route::get('/test_result', [PaymentController::class, 'testResult']);
